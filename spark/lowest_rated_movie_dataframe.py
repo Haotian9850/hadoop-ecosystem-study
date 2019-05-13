88026b7fdb7c5db6dcd09641e56fe9cfaf439aad
haotian <haotian@pop-os.localdomain>
2019-05-12 21:34:00 -0400

    minor corrections

--- spark/lowest_rated_movie_dataframe.py
+++ spark/lowest_rated_movie_dataframe.py
@@ -14,22 +14,22 @@ def parseInput(line):
     fields = line.split()
     return Row(movieID = int(fields[1], rating = float(fields[2])))
 
-    if __name__ = '__main__':
-        spark = SparkSession.builder.appName('PopularMovies').getOrCreate() #create a new SparkSession if none exists
-        movieNames = loadMovieNames()
+if __name__ = '__main__':
+    spark = SparkSession.builder.appName('PopularMovies').getOrCreate() #create a new SparkSession if none exists
+    movieNames = loadMovieNames()
 
-        lines = spark.sparkContext.textFile('hdfs:///user/maria_Dev_ml-100k/u.data')    #read input file
-        movies = line.map(parseInput)   #convert to RDD object
-        movieDataset = spark.createDataFrame(movies)    #construct DataFrame object
+    lines = spark.sparkContext.textFile('hdfs:///user/maria_Dev_ml-100k/u.data')    #read input file
+    movies = line.map(parseInput)   #convert to RDD object
+    movieDataset = spark.createDataFrame(movies)    #construct DataFrame object
 
-        averageRatings = movieDataset.groupBy('movieID').avg('rating')
-        counts = movieDataset.groupBy('movieID').count()
+    averageRatings = movieDataset.groupBy('movieID').avg('rating')
+    counts = movieDataset.groupBy('movieID').count()
         
-        averageAndCounts = counts.join(averageRatings, 'movieID')
+    averageAndCounts = counts.join(averageRatings, 'movieID')
 
-        topTen = averageAndCounts.orderBy('avg(rating)').take(10)
+    topTen = averageAndCounts.orderBy('avg(rating)').take(10)
 
-        for movie in topTen:
-            print(movieNames[movie[0]], movie[1], movie[2])
+    for movie in topTen:
+        print(movieNames[movie[0]], movie[1], movie[2])
         
-        spark.stop()
+    spark.stop()
