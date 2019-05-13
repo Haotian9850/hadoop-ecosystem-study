72e7c2abb63bf8644fdd4e4222f0ab9229b4eb63
haotian <haotian@pop-os.localdomain>
2019-05-12 21:06:06 -0400

    loadMovie function finished

--- spark/lowest_rated_movie_dataframe.py
+++ spark/lowest_rated_movie_dataframe.py
@@ -7,5 +7,27 @@ def loadMovieNames():
     with open('ml-100k/u.item') as f:
         for line in f:
             fields = line.split('|')
-            movieNames[int(fields[0])] = fields[1]
+            movieNames[int(fields[0])] = fields[1]  #map movieID to movieName
     return movieNames
+
+def parseInput(line):
+    fields = line.split()
+    return Row(movieID = int(fields[1], rating = float(fields[2])))
+
+    if __name__ = '__main__':
+        spark = SparkSession.builder.appName('PopularMovies').getOrCreate() #create a new SparkSession if none exists
+        movieNames = loadMovieNames()
+        lines = spark.sparkContext.textFile('hdfs:///user/maria_Dev_ml-100k/u.data')
+        movies = line.map(parseInput)
+        movieDataset = spark.createDataFrame(movies)
+        averageRatings = movieDataset.groupBy('movieID').avg('rating')
+        counts = movieDataset.groupBy('movieID').count()
+        
+        averageAndCounts = counts.join(averageRatings, 'movieID')
+
+        topTen = averageAndCounts.orderBy('avg(rating)').take(10)
+
+        for movie in topTen:
+            print(movieNames[movie[0]], movie[1], movie[2])
+        
+        spark.stop()
