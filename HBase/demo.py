cc0879528baafafa7d5b4b77c74352513fa98922
haotian <haotian@pop-os.localdomain>
2019-05-24 14:28:11 -0700

    HBase access with Python thru REST

--- HBase/demo.py
+++ HBase/demo.py
@@ -4,5 +4,35 @@
 
     Key: UserID
     Column family: rating 50 for movie 20, rating 29 for movie 22, etc...
+
+    Architecture:
+        Python client -> REST service -> HBase and HDFS
+
+    Backend steps: start HBase (VM) -> add port forwarding -> start REST service (/usr/hdp/current/hbase-master/bin/hbase-daemon.sh start rest -p 8000 -infoport 8001) 
 '''
+
+from starbase import Connection
+
+c = Connection('127.0.0.1', '8000')
+
+ratings = c.table('ratings')    
+
+if ratings.exists():
+    print('Dropping existing rating table \n')
+    ratings.drop()
+
+ratings.create('rating')    #creates a column family
+
+print('Parsing the ml-100k rating data... \n')
+ratingFile = open('e:/Downloads/ml-100k/ml-100k/u.data', 'r')
+
+batch = ratings.batch()
+
+for line in ratingFile:
+    (userID, movieID, rating, timeStamp) = line.split()
+    batch.update(userID, {'rating' : {movieID : rating}})
+
+ratingFile.close()
+
+
 
