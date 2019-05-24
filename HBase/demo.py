6f5f1c2da32c094e53b89494ff8d859983802379
haotian <haotian@pop-os.localdomain>
2019-05-24 14:14:41 -0700

    HBase python demo

---
+++ HBase/demo.py
@@ -0,0 +1,8 @@
+'''
+    Create a HBase table for movie ratings by user
+    -> Quickly query it for individual users (example of sparse data)
+
+    Key: UserID
+    Column family: rating 50 for movie 20, rating 29 for movie 22, etc...
+'''
+
